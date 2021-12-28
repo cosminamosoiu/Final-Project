ac0a9c360c1ccf37c58e19a92987bb9af6b8911b
cosminamosoiu <cosminutza.t@gmail.com>
2021-12-28 14:06:07 +0200

    Created the react app and added the navbar

---
+++ client/my-app/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems=[
+     {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+     },
+     {
+        title: 'Appointments',
+        url: '#',
+        cName: 'nav-links'
+     },
+     {
+        title: 'Therapists',
+        url: '#',
+        cName: 'nav-links'
+     },
+     {
+        title: 'Patients',
+        url: '#',
+        cName: 'nav-links'
+     },
+     {
+        title: 'Users',
+        url: '#',
+        cName: 'nav-links'
+     },
+
+]
